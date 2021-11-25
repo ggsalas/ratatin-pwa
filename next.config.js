280fd3ff278767827a51bafe82695c53d25f8fa5
Gabriel Salas <ggsalas@gmail.com>
2021-11-25 19:43:25 -0300

    service worker working with database

--- next.config.js
+++ next.config.js
@@ -1,15 +1,7 @@
-const withPWA = require('next-pwa')
-const runtimeCaching = require('next-pwa/cache')
+// const withPWA = require('next-pwa')
+// const runtimeCaching = require('next-pwa/cache')
 const withPlugins = require('next-compose-plugins')
 
 const withTM = require('next-transpile-modules')(['localbase']) // pass the modules you would like to see transpiled
 
-module.exports = withPlugins([withTM, withPWA], {
-  pwa: {
-    runtimeCaching,
-    disable: process.env.NODE_ENV === 'development',
-    // dest: 'public', // comment out this line
-    register: true,
-    sw: '/sw.js',
-  },
-})
+module.exports = withPlugins([withTM])
