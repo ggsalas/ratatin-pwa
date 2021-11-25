513ed75ee782072b8c04fdf83b56efb018976f0f
Gabriel Salas <ggsalas@gmail.com>
2021-11-25 20:39:00 -0300

    SW getting news each hour

--- public/sw.js
+++ public/sw.js
@@ -1,23 +1,57 @@
 self.importScripts('./localbaase.min.js')
 
-// const TOKEN_KEY = '__RATATIN_TOKEN__'
-
-async function pullConsole(interval) {
-  // setTimeout(() => pullConsole(interval), interval)
-  const db = new Localbase('ratatin')
-  const people = await db
-    .collection('people')
-    .orderBy('ratatinUpdatedAt', 'desc')
-    .get()
-  // const token = localStorage.getItem(TOKEN_KEY)
-  // const response = await fetch(`/api/people?token=${token}`)
-
-  // const data = await response.json()
-  console.log('.......................timeout', people)
+const USER = 'ratatin-user'
+const RATATIN_STATUS = {
+  match: 'match',
+  sendedLike: 'sendedLike',
+  sendedPass: 'sendedPass',
+  undefined: 'undefined',
 }
 
-self.addEventListener('install', function (event) {
-  console.log('Hello world from the Service Worker 🤙')
+async function getToken() {
+  let db = new Localbase('ratatin')
+  const user = await db.collection('user').doc(USER).get()
 
-  pullConsole(1000 * 10)
+  return user?.token ?? null
+}
+
+async function getNewData(interval) {
+  setTimeout(() => getNewData(interval), interval)
+
+  try {
+    const token = await getToken()
+
+    // Get new people and likes
+    const peopleResponse = await fetch(`/api/people?token=${token}`)
+    const likesResponse = await fetch(`/api/likes?token=${token}`)
+    const newPeople = await peopleResponse.json()
+    const newLikes = await likesResponse.json()
+
+    // Save them
+    const db = new Localbase('ratatin')
+
+    newPeople.data.results.forEach((person) => {
+      db.collection('people').add(
+        {
+          ...person,
+          ratatinUpdatedAt: Date.now(),
+          ratatinStatus: RATATIN_STATUS.undefined,
+        },
+        person.user._id
+      )
+    })
+
+    newLikes.data.results.forEach((like) => {
+      db.collection('likes').add(
+        { ...like, ratatinUpdatedAt: Date.now() },
+        like.user._id
+      )
+    })
+  } catch (err) {
+    console.error('error on SW', err)
+  }
+}
+
+self.addEventListener('install', function () {
+  getNewData(1000 * 60 * 60)
 })
